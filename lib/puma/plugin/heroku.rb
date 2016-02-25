1156d8d5dc6dbfbba31a21aab2310d9177d6a663
Evan Phoenix <evan@phx.io>
2016-02-25 14:30:08 -0800

    defined? has the strangest precidence

--- lib/puma/plugin/heroku.rb
+++ lib/puma/plugin/heroku.rb
@@ -13,7 +13,7 @@ Puma::Plugin.create do
       c.workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 
       c.on_worker_boot do
-        if defined? ::ActiveRecord && ::ActiveRecord::Base
+        if defined?(::ActiveRecord) && defined?(::ActiveRecord::Base)
           # Worker specific setup for Rails 4.1+
           # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
           ActiveRecord::Base.establish_connection
